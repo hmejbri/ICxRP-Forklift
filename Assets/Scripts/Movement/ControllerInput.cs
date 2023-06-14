bd268aac129f3ffaf1992a2e1fbecec71d00df59
ArctosFI <arttu.niemi@live.fi>
2023-06-14 12:19:07 +0300

    Controller inputs interface
    Script that translates controller inputs to unity variables

---
+++ Assets/Scripts/Movement/ControllerInput.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ControllerInput : MonoBehaviour
+{
+    [HideInInspector] public int gearValue { get; private set; }
+    [HideInInspector] public float[] joystickValues { get; private set; }
+
+    public string portName;
+    SerialPort port;
+
+    private void Start()
+    {
+        port = new SerialPort(portName);
+        port.Open();
+        joystickValues = new float[] { 0, 0, 0, 0 };
+    }
+
+    private void Update()
+    {
+        //Reading controller values from serial port
+        string input = port.ReadLine();
+        string[] inputValues = input.Split("|");
+
+        //Parsing the controller values to usable variables
+        gearValue = int.Parse(inputValues[0]);
+        for (int i = 1; i <= 4; i++)
+        {
+            joystickValues[i - 1] = float.Parse(inputValues[i]);
+        }
+    }
+}
