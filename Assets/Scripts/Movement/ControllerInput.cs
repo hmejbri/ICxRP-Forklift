a6db93418956d7c99b7c4f0cd8f03b1cc3d786d1
ArctosFI <arttu.niemi@live.fi>
2023-09-19 17:08:58 +0300

    working version
    The code can auto assign correct port to use with arduino and recognize different errors

--- Assets/Scripts/Movement/ControllerInput.cs
+++ Assets/Scripts/Movement/ControllerInput.cs
@@ -1,8 +1,10 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using System.IO.Ports;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 public class ControllerInput : MonoBehaviour
 {
@@ -10,22 +12,54 @@ public class ControllerInput : MonoBehaviour
     [HideInInspector] public float[] joystickValues { get; private set; }
     [HideInInspector] public int errorCode { get; private set; }
 
-    public string portName;
+    [SerializeField] string portName;
+    List<string> portNames;
     SerialPort port;
+    string input;
 
     Regex regexString = new Regex(@"^-?[0-1](?:\|-?(?:0\.[0-9]{2}|1\.00)){4}$");
 
     private void Start()
     {
-        port = new SerialPort(portName);
-        port.Open();
         joystickValues = new float[] { 0, 0, 0, 0 };
+
+        //If COM-port is not set manually, find and assing correct port name
+        if (portName == "")
+        {
+            portNames = GetSerialPortCaptions();
+
+            foreach (string _portName in portNames)
+            {
+                if (_portName.Contains("CH340"))
+                {
+                    portName = _portName.Substring(_portName.IndexOf('(') + 1, _portName.IndexOf(')') - _portName.IndexOf('(') - 1);
+                }
+            }
+
+            if (portName == "") { errorCode = 1; return; } //No device found
+        }
+
+        port = new SerialPort(portName);
     }
 
     private void Update()
     {
         //Reading controller values from serial port
-        string input = port.ReadLine();
+        if (port == null) { return; }
+
+        try
+        {
+            if (!port.IsOpen)
+            {
+                port.Open();
+            }    
+        }
+        catch (IOException)
+        {
+            errorCode = 2; //Serial port already in use
+            return;
+        }
+
         if (regexString.IsMatch(input))
         {
             string[] inputValues = input.Split("|");
@@ -43,5 +77,46 @@ public class ControllerInput : MonoBehaviour
         {
             errorCode = 3; //Invalid dataformat
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (port.IsOpen) { input = port.ReadLine(); }
+    }
+
+    public static List<string> GetSerialPortCaptions()
+    {
+        List<string> captions = new List<string>();
+
+        Process process = new Process();
+        process.StartInfo.FileName = "powershell.exe";
+        process.StartInfo.Arguments = @"Get-WmiObject -Class Win32_PnPEntity | Where-Object { $_.Caption -like '*(COM*' } | Select-Object -ExpandProperty Caption";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            UnityEngine.Debug.LogWarning("PowerShell command exited with error code: " + process.ExitCode);
+        }
+
+        if (!string.IsNullOrEmpty(output))
+        {
+            string[] lines = output.Split('\n');
+            foreach (string line in lines)
+            {
+                string trimmedLine = line.Trim();
+                if (!string.IsNullOrEmpty(trimmedLine))
+                {
+                    captions.Add(trimmedLine);
+                }
+            }
+        }
+
+        return captions;
     }
 }
